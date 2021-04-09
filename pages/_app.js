32fb443beafd25fc22bc93f9417a26e2327660fd
Pedro Martins <pedrovrima@gmail.com>
2021-04-09 16:45:33 -0300

    fixes page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/index.css'
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+  <Head>
+  <title>OAMa - Observatório de Aves da Mantiqueira</title>
+
+  </Head>
+  <Component {...pageProps} /></>)
 }
 
 export default MyApp
