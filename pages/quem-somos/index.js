e82c92611155b4ad5b3d86cbd81c0fe4cbff63df
Pedro Martins <pedrovrima@gmail.com>
2021-04-20 16:50:20 -0300

    transforms first paragraph in title

--- pages/quem-somos/index.js
+++ pages/quem-somos/index.js
@@ -25,166 +25,171 @@ export default function QuemSomos() {
       </div>
 
       <div className="mt-16">
-        <h1 class="w-full text-5xl font-bold leading-tight text-center text-gray-800">
-          Quem Somos{" "}
-        </h1>
-        <div class="w-full mb-4">
-          <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-        </div>
-
-        <div class="container text-center text-xl max-w-4xl mx-auto mb-12">
-          <p class="text-gray-600 px-8 md:px-0 mb-2">
-            O OAMa é movido por uma pequena equipe de trabalho e uma grande rede
-            de apoio.
-          </p>
-        </div>
-        <div class="container mb-12 max-w-4xl mx-auto ">
         <div className="mb-24">
-          <p className="text-4xl text-center font-bold text-gray-800">Equipe</p>
+          <h1 class="w-full text-5xl font-bold leading-tight text-center text-gray-800">
+            Quem Somos{" "}
+          </h1>
           <div class="w-full mb-4">
             <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
           </div>
-          <p class="text-gray-600 px-8 md:px-0 mb-2 text-center text-xl">
-            {" "}
-            Nossa equipe de trabalho, que coordena e executa as atividades
-            continuamente
-          </p>
 
-          <div class="flex md:flex-row-reverse mb-1  justify-around items-center   flex-wrap">
-            <div class="w-3/5  sm:w-1/4 p-2 sm:p-6">
-              <div>
-                <Image
-                  className="rounded-full border-gray-200 border-8"
-                  src="/AffonsoSouza.jpeg"
-                  layout="responsive"
-                  width={50}
-                  objectFit="cover"
-                  height={50}
-                ></Image>
+          <div class="container text-center text-2xl max-w-4xl mx-auto mb-12">
+            <p class="text-gray-800 px-8 md:px-0 ">
+              O OAMa é movido por uma pequena equipe de trabalho e uma grande
+              rede de apoio.
+            </p>
+          </div>
+        </div>
+        <div class="container mb-12 max-w-4xl mx-auto ">
+          <div className="mb-24">
+            <p className="text-4xl text-center font-bold text-gray-800">
+              Equipe
+            </p>
+            <div class="w-full mb-4">
+              <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+            </div>
+            <p class="text-gray-600 px-8 md:px-0 mb-2 text-center text-xl">
+              {" "}
+              Nossa equipe de trabalho, que coordena e executa as atividades
+              continuamente
+            </p>
+
+            <div class="flex md:flex-row-reverse mb-1  justify-around items-center   flex-wrap">
+              <div class="w-3/5  sm:w-1/4 p-2 sm:p-6">
+                <div>
+                  <Image
+                    className="rounded-full border-gray-200 border-8"
+                    src="/AffonsoSouza.jpeg"
+                    layout="responsive"
+                    width={50}
+                    objectFit="cover"
+                    height={50}
+                  ></Image>
+                </div>
+              </div>
+
+              <div class="w-full sm:w-2/3 p-6">
+                <h3 class="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-1">
+                  Affonso Souza{" "}
+                </h3>
+                <h4 class="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
+                  Coordenador de monitoramento e pesquisa{" "}
+                </h4>
+
+                <p class="text-gray-600 ">
+                  Biólogo com mestrado em ecologia. Trabalha com aves desde
+                  2012, com especial interesse em manejo de fauna e ecologia
+                  aplicada.
+                </p>
+                <a
+                  className="underline"
+                  target="_blank"
+                  href="http://lattes.cnpq.br/6486611783204721"
+                >
+                  Lattes
+                </a>
+                <a
+                  className="underline ml-1"
+                  target="_blank"
+                  href="mailto:asouza@oama.eco.br
+"
+                >
+                  E-mail
+                </a>
               </div>
             </div>
 
-            <div class="w-full sm:w-2/3 p-6">
-              <h3 class="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-1">
-                Affonso Souza{" "}
-              </h3>
-              <h4 class="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-                Coordenador de monitoramento e pesquisa{" "}
-              </h4>
+            <div class="flex md:flex-row mb-1 justify-center items-center   flex-wrap">
+              <div class="w-3/5  sm:w-1/4 p-2 sm:p-6">
+                <div>
+                  <Image
+                    className="rounded-full"
+                    layout="responsive"
+                    src="/LuizaFigueira.jpg"
+                    width={200}
+                    objectFit="cover"
+                    height={200}
+                  ></Image>
+                </div>
+              </div>
 
-              <p class="text-gray-600 ">
-                Biólogo com mestrado em ecologia. Trabalha com aves desde 2012,
-                com especial interesse em manejo de fauna e ecologia aplicada.
-              </p>
-              <a
-                className="underline"
-                target="_blank"
-                href="http://lattes.cnpq.br/6486611783204721"
-              >
-                Lattes
-              </a>
-              <a
-                className="underline ml-1"
-                target="_blank"
-                href="mailto:asouza@oama.eco.br
+              <div class="w-full sm:w-2/3 p-6">
+                <h3 class="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-1">
+                  Luiza Figueira{" "}
+                </h3>
+                <h4 class="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
+                  Coordenadora Geral{" "}
+                </h4>
+
+                <p class="text-gray-600">
+                  Bióloga com mestrado em ecologia, trabalha com aves desde 2012
+                  e tem maior interesse em biologia da conservação e restauração
+                  ecológica.
+                </p>
+                <a
+                  className="underline"
+                  target="_blank"
+                  href="http://lattes.cnpq.br/6486611783204721"
+                >
+                  Lattes
+                </a>
+                <a
+                  className="underline ml-1"
+                  target="_blank"
+                  href="mailto:asouza@oama.eco.br
 "
-              >
-                E-mail
-              </a>
-            </div>
-          </div>
-
-          <div class="flex md:flex-row mb-1 justify-center items-center   flex-wrap">
-            <div class="w-3/5  sm:w-1/4 p-2 sm:p-6">
-              <div>
-                <Image
-                  className="rounded-full"
-                  layout="responsive"
-                  src="/LuizaFigueira.jpg"
-                  width={200}
-                  objectFit="cover"
-                  height={200}
-                ></Image>
+                >
+                  E-mail
+                </a>
               </div>
             </div>
 
-            <div class="w-full sm:w-2/3 p-6">
-              <h3 class="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-1">
-                Luiza Figueira{" "}
-              </h3>
-              <h4 class="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-                Coordenadora Geral{" "}
-              </h4>
+            <div class="flex md:flex-row-reverse mb-8  justify-around items-center   flex-wrap">
+              <div class="w-3/5  sm:w-1/4 p-2 sm:p-6">
+                <div>
+                  <Image
+                    className="rounded-full border-gray-200 border-8"
+                    src="/PedroMartins.jpg"
+                    layout="responsive"
+                    width={50}
+                    objectFit="cover"
+                    height={50}
+                  ></Image>
+                </div>
+              </div>
 
-              <p class="text-gray-600">
-                Bióloga com mestrado em ecologia, trabalha com aves desde 2012 e
-                tem maior interesse em biologia da conservação e restauração
-                ecológica.
-              </p>
-              <a
-                className="underline"
-                target="_blank"
-                href="http://lattes.cnpq.br/6486611783204721"
-              >
-                Lattes
-              </a>
-              <a
-                className="underline ml-1"
-                target="_blank"
-                href="mailto:asouza@oama.eco.br
+              <div class="w-full sm:w-2/3 p-6">
+                <h3 class="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-1">
+                  Pedro Martins{" "}
+                </h3>
+                <h4 class="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
+                  Coordenador de dados{" "}
+                </h4>
+
+                <p class="text-gray-600 ">
+                  Biólogo com mestrado em ecologia e programador. Trabalha com
+                  aves desde 2010 e tem maior interesse na coleta, manejo e
+                  análise de dados ornitológicos.
+                </p>
+                <a
+                  className="underline"
+                  target="_blank"
+                  href="http://lattes.cnpq.br/1082589394374306
 "
-              >
-                E-mail
-              </a>
-            </div>
-          </div>
+                >
+                  Lattes
+                </a>
+                <a
+                  className="underline ml-1"
+                  target="_blank"
+                  href="mailto:pmartins@oama.eco.br
 
-          <div class="flex md:flex-row-reverse mb-8  justify-around items-center   flex-wrap">
-            <div class="w-3/5  sm:w-1/4 p-2 sm:p-6">
-              <div>
-                <Image
-                  className="rounded-full border-gray-200 border-8"
-                  src="/PedroMartins.jpg"
-                  layout="responsive"
-                  width={50}
-                  objectFit="cover"
-                  height={50}
-                ></Image>
+"
+                >
+                  E-mail
+                </a>
               </div>
             </div>
-
-            <div class="w-full sm:w-2/3 p-6">
-              <h3 class="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-1">
-                Pedro Martins{" "}
-              </h3>
-              <h4 class="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-                Coordenador de dados{" "}
-              </h4>
-
-              <p class="text-gray-600 ">
-                Biólogo com mestrado em ecologia e programador. Trabalha com
-                aves desde 2010 e tem maior interesse na coleta, manejo e
-                análise de dados ornitológicos.
-              </p>
-              <a
-                className="underline"
-                target="_blank"
-                href="http://lattes.cnpq.br/1082589394374306
-"
-              >
-                Lattes
-              </a>
-              <a
-                className="underline ml-1"
-                target="_blank"
-                href="mailto:pmartins@oama.eco.br
-
-"
-              >
-                E-mail
-              </a>
-            </div>
-          </div>
           </div>
           <div class="container  max-w-4xl mx-auto mb-24 ">
             <p className="text-4xl text-center font-bold text-gray-800">
@@ -333,37 +338,37 @@ export default function QuemSomos() {
               burocraticamente.{" "}
             </p>
             <div className="p-6">
-            <p className="text-gray-800 mb-2 text-center font-bold text-xl">
-              Diretoria
-            </p>
-            <ul className="text-center text-gray-700">
-              <li>
-                <strong>Presidente:</strong> Luiza Figueira
-              </li>
-              <li>
-                <strong>Secretário:</strong> Pedro Martins
-              </li>
-              <li>
-                <strong>Tesoureiro:</strong> Affonso Souza
-              </li>
-            </ul>
+              <p className="text-gray-800 mb-2 text-center font-bold text-xl">
+                Diretoria
+              </p>
+              <ul className="text-center text-gray-700">
+                <li>
+                  <strong>Presidente:</strong> Luiza Figueira
+                </li>
+                <li>
+                  <strong>Secretário:</strong> Pedro Martins
+                </li>
+                <li>
+                  <strong>Tesoureiro:</strong> Affonso Souza
+                </li>
+              </ul>
             </div>
             <div className="flex flex-col items-center p-6">
-            <p className="text-gray-800 text-center mb-2 font-bold text-xl">
-              Associados
-            </p>
-            <ul className="text-gray-700">
-              <li>Affonso Henrique de Souza, biólogo</li>
-              <li>Felipe Cotrim, jornalista</li>
-              <li>Joaquim Mendonça, engenheiro florestal</li>
-              <li>Julia Rodrigues, fotógrafa</li>
-              <li>Lino Matheus de Sá Pereira, ambientalista</li>
-              <li>Luiza Figueira, bióloga</li>
-              <li>Nívea Leite, artista plástica</li>
-              <li>Pedro Martins, biólogo</li>
-              <li>Raquel Justo, bióloga</li>
-              <li>Renata Miwa, artista plástica e designer</li>
-            </ul>
+              <p className="text-gray-800 text-center mb-2 font-bold text-xl">
+                Associados
+              </p>
+              <ul className="text-gray-700">
+                <li>Affonso Henrique de Souza, biólogo</li>
+                <li>Felipe Cotrim, jornalista</li>
+                <li>Joaquim Mendonça, engenheiro florestal</li>
+                <li>Julia Rodrigues, fotógrafa</li>
+                <li>Lino Matheus de Sá Pereira, ambientalista</li>
+                <li>Luiza Figueira, bióloga</li>
+                <li>Nívea Leite, artista plástica</li>
+                <li>Pedro Martins, biólogo</li>
+                <li>Raquel Justo, bióloga</li>
+                <li>Renata Miwa, artista plástica e designer</li>
+              </ul>
             </div>
           </div>
         </div>
