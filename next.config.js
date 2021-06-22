64cef5bb505dc1c5044b5bb33f1ef20852537b30
Pedro Martins <pedrovrima@gmail.com>
2021-06-22 13:04:41 -0300

    changes config for blog

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+  })
+
 module.exports={
     i18n: {
         // These are all the locales you want to support in
@@ -5,5 +9,6 @@ module.exports={
         locales: ['en-US',  'pt-BR'],
         // This is the default locale you want to be used when visiting
         // a non-locale prefixed path e.g. `/hello`
-        defaultLocale: 'pt-BR',}
+        defaultLocale: 'pt-BR',},
+        pageExtensions: ['js', 'jsx', 'mdx']
 }
