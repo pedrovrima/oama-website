c425f7bc6c8071134d837345f1b044d1cbd8ca31
Pedro Martins <pedrovrima@gmail.com>
2021-06-25 17:57:31 -0300

    fixes media query

--- _includes/nav.js
+++ _includes/nav.js
@@ -22,9 +22,9 @@ export default function Nav() {
 
   return (
     //   <nav className="bg-gray-800">
-    //   <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    //   <div className="max-w-7xl mx-auto px-2 md:px-6 lg:px-8">
     //     <div className="relative flex items-center justify-between h-16">
-    //       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    //       <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
     //         <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
     //           <span className="sr-only">Open main menu</span>
     //           <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -35,12 +35,12 @@ export default function Nav() {
     //           </svg>
     //         </button>
     //       </div>
-    //       <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+    //       <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
     //         <div className="flex-shrink-0 flex items-center">
     //           <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"/>
     //           <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"/>
     //         </div>
-    //         <div className="hidden sm:block sm:ml-6">
+    //         <div className="hidden md:block md:ml-6">
     //           <div className="flex space-x-4">
     //             <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900">Dashboard</a>
     //             <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700">Team</a>
@@ -49,7 +49,7 @@ export default function Nav() {
     //           </div>
     //         </div>
     //       </div>
-    //       <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+    //       <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
     //         <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
     //           <span className="sr-only">View notifications</span>
     //           <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -74,7 +74,7 @@ export default function Nav() {
     //     </div>
     //   </div>
 
-    //   <div className="hidden sm:hidden">
+    //   <div className="hidden md:hidden">
     //     <div className="px-2 pt-2 pb-3 space-y-1">
     //       <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900">Dashboard</a>
     //       <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700">Team</a>
@@ -130,7 +130,7 @@ export default function Nav() {
           </div>
         </Link>
         {/* Hamburguer Menu  */}
-        <div ref={ref} className="block sm:hidden pr-4 font-bold">
+        <div ref={ref} className="block md:hidden pr-4 font-bold">
           <button
             onClick={() => {
               console.log(open);
@@ -177,7 +177,7 @@ export default function Nav() {
             </div>
           </div>
         </div>
-        <div className="hidden sm:flex flex-row items-center">
+        <div className="hidden md:flex flex-row items-center">
           <div className=" px-8 flex flex-row">
             <Link href="/quem-somos">
               <div className=" mr-4 cursor-pointer">
