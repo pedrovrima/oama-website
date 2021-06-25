e14a31db1a011c04fd65284acf2b3bc8f7b2384b
Pedro Martins <pedrovrima@gmail.com>
2021-06-25 15:51:19 -0300

    hamburguer menu

--- _includes/nav.js
+++ _includes/nav.js
@@ -12,8 +12,6 @@ export default function Nav() {
     setOpen(false);
   });
 
-  const changeOpen = (state) => setOpen(!state);
-
   useEffect(
     () =>
       window.addEventListener("scroll", (e) =>
@@ -117,24 +115,27 @@ export default function Nav() {
     >
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <Link href="/">
-        <div className="pl-4 flex items-center">
-          <a
-            className="flex toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-            href="#"
-          >
-            <Image
-              src="/logo.png"
-              width={50}
-              height={50}
-              objectFit={"cover"}
-            ></Image>
-          </a>
-        </div>
+          <div className="pl-4 flex items-center">
+            <a
+              className="flex toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+              href="#"
+            >
+              <Image
+                src="/logo.png"
+                width={50}
+                height={50}
+                objectFit={"cover"}
+              ></Image>
+            </a>
+          </div>
         </Link>
         {/* Hamburguer Menu  */}
-        {/* <div className="block hidden pr-4">
+        <div ref={ref} className="block sm:hidden pr-4 font-bold">
           <button
-            onClick={() => changeOpen(open)}
+            onClick={() => {
+              console.log(open);
+              setOpen(!open);
+            }}
             id="nav-toggle"
             className="flex items-center p-1 text-white hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
@@ -143,65 +144,64 @@ export default function Nav() {
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
-        </div> */}
-        <div
-          className={`flex items-center w-auto
+          <div
+            className={`flex items-center w-auto
           mt-0 bg-transparent text-black p-0 z-20 justify-center`}
-          id="nav-content"
-          ref={ref}
-        >
-          {/* For Hamburguer Menu */}
-          {/* <div
-          className={`w-full flex-grow lg:flex items-end lg:w-auto ${
-            open ? "" : "hidden"
-          } mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20 justify-end`}
-          id="nav-content"
-          ref={ref}
-        > */}
-
-          {/* <ul className="list-reset lg:flex justify-end flex-1 items-center">
-
-            <li className="mr-3">
-              <a
-                className="inline-block py-2 px-4 text-black font-bold no-underline"
-                href="#"
-              >
-                Active
-              </a>
-            </li>
-            <li className="mr-3">
-              <a
-                className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                href="#"
-              >
-                link
-              </a>
-            </li>
-            <li className="mr-3">
-              <a
-                className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                href="#"
-              >
-                link
-              </a>
-            </li>
-          </ul> */}
-          <Link href="/quem-somos">
-            <div className="px-8 cursor-pointer">
-              <p className="text-gray-50  hover:underline font-bold">Quem somos</p>
-            </div>
-
-          </Link>
-                  <Link href="/apoie">
-
-          <div className="justify-end flex items-end">
-            <button
-              id="navAction"
-              className="mx-auto lg:mx-0 mr-2 hover:underline bg-yellow-400 text-gray-800 font-bold rounded-full mt-0 py-2  px-8 shadow  focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            id="nav-content"
+          >
+            {/* For Hamburguer Menu */}
+            <div
+              className={` w-full flex-grow absolute top-14 lg:flex items-end lg:w-auto ${
+                open ? "" : "hidden"
+              } mt-2 lg:mt-0 bg-white lg:bg-transparent text-gray-900  lg:p-0 z-20 justify-end`}
+              id="nav-content"
+              ref={ref}
             >
-              Apoie
-            </button>
+              <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                <li className="inline-block w-full   no-underline hover:text-gray-800 hover:text-underline py-2  px-4">
+                  <Link href="/quem-somos">Quem somos</Link>
+                </li>
+                <li className="inline-block w-full   no-underline hover:text-gray-800 hover:text-underline py-2  px-4">
+                  <Link
+                    href="/blog"
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li className="inline-block bg-yellow-400 w-full  no-underline hover:text-gray-800 hover:text-underline py-2 px-4">
+                  <Link className="" href="/apoie">
+                    Apoie
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
+        </div>
+        <div className="hidden sm:flex flex-row items-center">
+          <div className=" px-8 flex flex-row">
+            <Link href="/quem-somos">
+              <div className=" mr-4 cursor-pointer">
+                <p className="text-gray-50  hover:underline font-bold">
+                  Quem somos
+                </p>
+              </div>
+            </Link>
+
+            <Link href="/blog">
+              <div className=" mr-4 cursor-pointer">
+                <p className="text-gray-50  hover:underline font-bold">Blog</p>
+              </div>
+            </Link>
+          </div>
+          <Link href="/apoie">
+            <div className="justify-end flex items-end">
+              <button
+                id="navAction"
+                className="mx-auto lg:mx-0 mr-2 hover:underline bg-yellow-400 text-gray-800 font-bold rounded-full mt-0 py-2  px-8 shadow  focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Apoie
+              </button>
+            </div>
           </Link>
         </div>
       </div>
