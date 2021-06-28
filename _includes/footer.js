7fe0398222af87cbb72a958979c353322592c6c1
Pedro Martins <pedrovrima@gmail.com>
2021-06-27 22:27:26 -0300

    starting footer

--- _includes/footer.js
+++ _includes/footer.js
@@ -1,3 +1,16 @@
 export default function Footer() {
-    return <footer><p>©2020 | Footer</p></footer>
-  }
+  return (
+    <footer>
+      <div className="flex flex-col mt-16 p-4 bg-gray-900 w-full md:flex-row">
+        <div className="w-full flex flex-col items-center text-gray-100 md:w-1/2">
+          <p>Observatório de Aves da Mantiqueira</p>
+          <p>CNPJ: </p>
+          <p> contato@oama.eco.br</p>
+          
+
+        </div>
+        <div>oi</div>
+      </div>
+    </footer>
+  );
+}
--- _layouts/default.js
+++ _layouts/default.js
@@ -1,16 +1,17 @@
-import Head from 'next/head'
-import Header from '@includes/header'
-import Footer from '@includes/footer'
-import Nav from '@includes/nav'
+import Head from "next/head";
+import Header from "@includes/header";
+import Footer from "@includes/footer";
+import Nav from "@includes/nav";
 export default function DefaultLayout(props) {
   return (
     <main>
       <Head>
         <title>{props.title}</title>
-        <meta name='description' content={props.description}/>
+        <meta name="description" content={props.description} />
       </Head>
-      <Nav/>
+      <Nav />
       {props.children}
+      <Footer />
     </main>
-  )
+  );
 }
