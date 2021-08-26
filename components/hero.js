df8881cdfc546a2a3ae4ce7bb510de533880b91a
Pedro Martins <pedrovrima@gmail.com>
2021-08-26 19:29:03 -0300

    adds jucara video

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Hero() {
   return (
     //   Hero
@@ -21,11 +20,10 @@ export default function Hero() {
               Atlântica
             </p>
             <Link href="/apoie">
-
-            <button className="mx-auto md:mx-0 hover:underline bg-yellow-500 text-gray-900 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out ">
-              Apoie
-            </button>
-</Link>
+              <button className="mx-auto md:mx-0 hover:underline bg-yellow-500 text-gray-900 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out ">
+                Apoie
+              </button>
+            </Link>
           </div>
           {/* <!--Right Col--> */}
           <div className="w-full md:w-3/5 py-6 text-center">
---
+++ pages/jucara/index.js
@@ -0,0 +1,36 @@
+import Nav from "@includes/nav";
+import { useEffect, useRef } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import Image from "next/image";
+import Head from "next/head";
+export default function Apoie() {
+  return (
+    <div className="pb-32">
+      <Head>
+        <title>Juçara - Conservação e Consumo</title>
+      </Head>
+
+      <Nav />
+      <div className=" relative hero-small md:hero min-h-1/2">
+        <div className="pt-32 lg:pb-16 md:pb-16 h-full  justify-items-center items-center">
+          <div className="absolute top-0 h-full w-full">
+            <Image
+              // className="h-32"
+              src="/heros/apoie.jpg"
+              layout="fill"
+              //   width={"2000"}
+              //   height={"1000"}
+              objectFit="cover"
+              objectPosition="center"
+              priority={true}
+            ></Image>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16 flex items-center justify-center">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/FtL8dOPeu6U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  );
+}
