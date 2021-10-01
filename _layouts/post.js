e88beaca8c5aefd8d5b8d640e8f82a317c92d95c
Pedro Martins <pedrovrima@gmail.com>
2021-10-01 18:51:56 -0300

    fixes small problem

--- _layouts/post.js
+++ _layouts/post.js
@@ -3,7 +3,7 @@ import { SmallHero } from "@includes/small-hero";
 import Title from "@includes/title";
 import Head from "next/head";
 import Link from "next/link";
-import {PostAuthorPost} from "@includes/post_author";
+import { PostAuthorPost } from "@includes/post_author";
 
 import Image from "next/image";
 
@@ -11,7 +11,7 @@ export default function PostLayout(props) {
   console.log(props);
   return (
     <DefaultLayout>
-      <SmallHero img_src={`blog/${props.hero?props.hero:"default.jpg"}`} />
+      <SmallHero img_src={`blog/${props.hero ? props.hero : "default.jpg"}`} />
 
       <Head>
         <title>{props.title}</title>
@@ -19,9 +19,11 @@ export default function PostLayout(props) {
       <article className="w-3/4 sm:max-w-4xl mx-auto">
         <Title title={props.title}></Title>
         <PostAuthorPost {...props}></PostAuthorPost>
-        <div className="blog-content list-disc container max-w-4xl mx-auto text-gray-600 md:px-0 mb-2" dangerouslySetInnerHTML={{ __html: props.content }} />
+        <div
+          className="blog-content list-disc container max-w-4xl mx-auto text-gray-600 md:px-0 mb-2"
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        />
       </article>
-
     </DefaultLayout>
   );
 }
