4524e236a5c196e8fd4f042774765b984415b822
Pedro Martins <pedrovrima@gmail.com>
2021-12-30 14:56:33 -0300

    adds conclude courses

--- pages/treinamento-cursos/index.jsx
+++ pages/treinamento-cursos/index.jsx
@@ -137,6 +137,60 @@ export default function Treinamento() {
           </div>
 
         </div>
+
+
+        <h2 className="w-full mt-12 mb-6 text-3xl font-bold leading-tight text-center text-gray-800">
+          Cursos Concluídos
+        </h2>
+        <div className="flex flex-col sm:flex-row ">
+          <div class="w-full sm:w-1/3 p-2 sm:p-6 my-auto">
+            <div>
+              <Image
+                placeholder="blur"
+                alt="Serra da Mantiqueira"
+                layout="responsive"
+                src="/sobre/Mantiqueira.jpg"
+                width={400}
+                objectFit="cover"
+                height={400}
+              />
+            </div>
+          </div>
+          <div className="w-full sm:w-2/3">
+              <p className="text-gray-600 text-lg font-bold">Nome do Curso</p>
+              <p className="text-gray-500 text-md font-bold">11/01/1991</p>
+              <p className="text-gray-500 text-md font-bold">Estação de Pesquisa Refúgio da Boa Vista</p>
+            <p className="text-gray-600 ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              eget condimentum felis. Nulla ac lacus nisi. Nam eget ultricies
+              leo. Pellentesque rutrum nibh vel nibh hendrerit ultrices.
+              Pellentesque convallis neque at vulputate tempor. Nulla congue
+              mattis lectus, placerat ullamcorper libero egestas a. Vivamus
+              fermentum ex in felis consectetur, et iaculis nulla malesuada. In
+              fringilla condimentum fringilla. Etiam eget pretium nulla. Vivamus
+              quis magna laoreet, aliquet leo nec, condimentum ipsum. Praesent
+              sollicitudin ante non neque tristique, id rutrum dui maximus.
+              Morbi id sem ut velit faucibus mattis. Nulla varius malesuada orci
+              ut dapibus. Ut faucibus gravida ornare. Proin ut pretium dui.
+              Donec tincidunt vitae risus eu ultricies. Suspendisse convallis
+              auctor lacus, vitae posuere quam feugiat ac. Duis a ex non tortor
+              lobortis molestie. Nam in lacus vel enim finibus bibendum. Duis
+              tincidunt porttitor lorem mollis semper. Mauris tincidunt ex in
+              ullamcorper blandit. Suspendisse pulvinar, mauris sit amet congue
+              fringilla, eros magna vestibulum nisl, a ullamcorper libero nisi
+              vel justo. Morbi a ultrices mi. Sed tempor dolor enim, quis cursus
+              enim auctor ac. In finibus tellus sit amet mauris varius, sit amet
+              fringilla mauris tempor. Maecenas pretium sagittis eros, ut
+              interdum mi pulvinar at. Nunc feugiat fermentum turpis, quis
+              finibus nunc rutrum eget. Suspendisse venenatis lacinia sem, sed
+              gravida lectus semper at. Donec volutpat aliquet nisi, eu pretium
+              augue rutrum eget. Integer id vehicula nibh. Aliquam tincidunt
+              pharetra risus. Cras semper erat nisl, id ornare <a className="underline text-yellow-600 cursor-pointer">Link</a>.
+            </p>
+          </div>
+
+        </div>
+
       </div>
     </div>
   );
