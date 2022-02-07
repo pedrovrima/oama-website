4f9c0c28222ef75fc99d3fa49682ac8e3047f86b
Pedro Martins <pedrovrima@gmail.com>
2022-02-07 14:24:40 -0300

    finalizes hospedagem page with pictures and final text

--- pages/estacao-de-pesquisa/index.js
+++ pages/estacao-de-pesquisa/index.js
@@ -201,6 +201,10 @@ export default function QuemSomos() {
               <br />
             </p>
           </div>
+          <div>
+<p className="text-lg px-8 font-bold">          Importante: Visando mitigar os contágios pelo novo coronavírus, recebemos apenas hóspedes com comprovação de pelo menos duas doses da vacina contra a covid-19 e sem sintomas da doença. O uso de máscaras é indispensável nas áreas de convivência coletiva da EP. </p>
+
+          </div>
         </div>
       </div>
 
@@ -210,13 +214,23 @@ export default function QuemSomos() {
 }
 
 const EPCarousel = (props) => {
-  const images = [{pic:"/sobre/anilhamento.jpeg",legend:"Legenda 1"}, {pic:"/sobre/crianca.jpg",legend:"Legenda 2"}];
+
+  const fotos=[
+  {pic:"/fotos_ep/aerea.jpeg",legend:"Foto aérea da EP.",},
+  {pic:"/fotos_ep/frontal.jpg",legend:"Foto frontal da EP.",},
+  {pic:"/fotos_ep/lateral.jpg",legend:"Vista lateral da EP.",},
+  {pic:"/fotos_ep/piscina.jpg",legend:"Piscina Natural."},
+
+  {pic:"/fotos_ep/acesso.jpg",legend:"Trilha de acesso à EP.",},
+  {pic:"/fotos_ep/noite.jpg",legend:"Vista noturna da EP, com Pedra Selada ao fundo.",},
+]
+  
   return (
-    <div className="flex group h-100 justify-center items-center sm:pr-8">
+    <div className="flex group h-100 justify-center items-center sm:px-8">
       <Carousel showArrows={true} showStatus autoPlay swipeable  emulateTouch infiniteLoop className="mt-2 bg-gray-700 mb-8 sm:mb-4  h-72 w-full sm:h-96 my-auto">
-        {images.map((img) => (
+        {fotos.map((img) => (
           <div className="h-72 w-full sm:h-96  relative">
-            <Image layout="fill" objectFit="contain" src={img.pic}></Image>
+            <Image alt={img.legend} layout="fill" objectFit="contain" src={img.pic}></Image>
             <div className="w-full flex absolute bottom-12 justify-center">
             <p className="w-1/3 opacity-80 sm:opacity-5 text-white bg-gray-700 rounded-md p-2 group-hover:opacity-80 ">{img.legend}</p>
             </div>
