a15d4c9386691e20f9be7a7c73c14204a7687d35
Pedro Martins <pedrovrima@gmail.com>
2022-07-02 17:08:49 -0300

    revamped "anilhamento demonstrativo" page

--- pages/anilhamento-demonstrativo/index.js
+++ pages/anilhamento-demonstrativo/index.js
@@ -34,9 +34,9 @@ export default function QuemSomos() {
         </div>
 
         <div className="mt-16">
-          <div className="mb-24">
+          <div className="mb-12">
             <h1 className="w-full text-5xl font-bold leading-tight text-center text-gray-800">
-              Hospedagem com anilhamento demonstrativo{" "}
+              Anilhamento demonstrativo{" "}
             </h1>
             <div className="w-full mb-4">
               <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
@@ -45,28 +45,47 @@ export default function QuemSomos() {
         </div>
         <div className="container mb-12 max-w-4xl mx-auto ">
           <div className="w-full p-6">
-            <h3 className="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-              Visite uma estação de pesquisa, conheça o trabalho de ornitólogos
-              em campo e apoie a conservação com ciência!{" "}
+            <h3 className="text-2xl text-justify-center text-gray-700 font-bold leading-none mb-12">
+              Você tem curiosidade de ver como é o trabalho dos biólogos que
+              estudam as aves? Ou o interesse de acompanhá-los na floresta
+              durante seu trabalho de campo? O anilhamento demonstrativo de aves
+              do OAMa é uma oportunidade única de viver a natureza e ver as aves
+              com um olhar científico!
             </h3>
-            <p className="text-gray-600 ">
-              O OAMa abre as portas de sua estação de pesquisa para amantes da
-              natureza e das aves conhecerem o trabalho de campo em nosso
-              monitoramento de longo-prazo da avifauna na Serra da Mantiqueira.
+            <p className="text-gray-600 mb-4 ">
+              Abrimos as portas da Estação de Pesquisa para uma manhã de campo
+              com o anilhamento demonstrativo e muita conversa sobre aves,
+              ciência e conservação. Nessa experiência você tem a chance de ver
+              de perto e fotografar algumas das mais de 100 espécies que
+              estudamos, e se encantar com a soltura dos animais após a coleta
+              de dados para estudo e monitoramento da avifauna local. E ainda
+              tem o prazer de acompanhar tudo isso ao longo de um café da manhã
+              bem da roça e ao ar livre.{" "}
             </p>
-            <AniCarousel/>
+            <AniCarousel className="mb-4" />
           </div>
+          <p className="text-gray-600 mb-4">
+            O anilhamento de aves é uma técnica de marcação desses animais pela
+            aplicação de anilhas de alumínio em suas patas. Cada uma possui um
+            código único, o que nos permite recapturar e acompanhar a mesma ave
+            por muitos anos. Assim, podemos conhecer mais sobre as espécies,
+            seus habitats e contribuir para sua conservação! Os anilhamentos
+            demonstrativos são momentos únicos de nosso monitoramento, nos quais
+            nos dedicamos a aproximar as pessoas da pesquisa científica através
+            da experiência em campo. Visamos proporcionar um contato mais
+            próximo com a beleza, delicadeza e importância das aves da Mata
+            Atlântica.
+          </p>
           <div className="w-full p-6">
             <h3 className="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-3">
               Quando{" "}
             </h3>
             <p className="text-gray-600 mb-4 ">
-              Todo último final de semana do mês, de sexta-feira à domingo.
-              Próximos eventos:
+              Todo último sábado do mês, de sexta-feira à domingo. Próximos
+              eventos:
               <ul>
-                <li className="text-gray-600">17/06/2022 - 19/06/2022</li>
-                <li className="text-gray-600">29/07/2022 - 31/07/2022</li>
-                <li className="text-gray-600">26/08/2022 - 28/08/2022</li>
+                <li className="text-gray-600">30/07/2022</li>
+                <li className="text-gray-600">27/08/2022</li>
               </ul>
             </p>
           </div>
@@ -87,102 +106,92 @@ export default function QuemSomos() {
           </div>
           <EPCarousel />{" "}
           <div className="w-full p-6">
-            <h3 className="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-              O que inclui{" "}
+            <h3 className="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-8">
+              Pacotes{" "}
             </h3>
-            <ul className="text-gray-600 mb-4 ">
-              <li className="text-gray-600">Duas noites de hospedagem</li>
-              <li className="text-gray-600">alimentação completa</li>
-              <li className="text-gray-600">
-                1 noite de apresentação sobre o OAMa
-              </li>
-              <li className="text-gray-600">
-                {" "}
-                1 manhã de visita guiada a uma estação de anilhamento de aves
-              </li>
-              <li className="text-gray-600">
-                {" "}
-                1 manhã de passarinhada na RPPN da Fazenda Boa Vista
-              </li>
-            </ul>
+
+            <div className="mb-12">
+              <h4 className="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
+                Café da manhã com as aves
+              </h4>
+
+              <ul className="text-gray-600 mb-2 ">
+                <li className="text-gray-600">
+                  Café da manhã ao ar livre durante toda manhã
+                </li>
+                <li className="text-gray-600">
+                  Acompanhamento do anilhamento demonstrativo
+                </li>
+                <li className="text-gray-600">
+                  Uso recreativo das trilhas e espaços da reserva
+                </li>
+              </ul>
+              <p>
+                <span className="font-bold">Quando?</span> Aos sábados (veja
+                datas acima) das 7:00 às 11:00
+              </p>
+              <p>
+                <span className="font-bold">Quanto?</span> R$ 50,00 por adulto e
+                R$ 25,00 por criança/adolescente
+              </p>
+            </div>
+
+            <div className="mb-4">
+              <h4 className="text-xl text-justify-center text-gray-800 font-bold leading-none mb-3">
+                Hospedagem com ciência
+              </h4>
+
+              <ul className="text-gray-600 mb-4 ">
+                <li>Inclui duas diárias e alimentação completa</li>
+                <li>Palestra de apresentação do OAMa</li>
+                <li className="text-gray-600">
+                  Passarinhada pelas trilhas da reserva{" "}
+                </li>
+                <li className="text-gray-600">
+                  Acompanhamento do anilhamento demonstrativo
+                </li>
+                <li className="text-gray-600">
+                  Uso recreativo das trilhas e espaços da reserva
+                </li>
+              </ul>
+              <p>
+                <span className="font-bold">Quando?</span> De sexta-feita (14h)
+                a domingo (10h) - veja datas acima
+              </p>
+              <p>
+                <span className="font-bold">Quanto?</span> R$ 900,00 o quarto
+                individual e R$ 1260 quarto para duas pessoas
+              </p>
+            </div>
           </div>
           <div className="w-full p-6">
-            <h3 className="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-              Programação{" "}
-            </h3>
-            <h4 className="text-xl text-justify-center text-gray-800 font-bold leading-none mb-1">
-              Sexta-Feira{" "}
-            </h4>
-            <ul className="text-gray-600 mb-4 ">
-              <li className="text-gray-600">14:00-17:00 - Check-in </li>
-              <li className="text-gray-600">
-                18:00 - Apresentação sobre o OAMa
-              </li>
-              <li className="text-gray-600">19:00 - Jantar</li>
-            </ul>
-
-            <h4 className="text-xl text-justify-center text-gray-800 font-bold leading-none mb-1">
-              Sábado{" "}
-            </h4>
-            <ul className="text-gray-600 mb-4 ">
-              <li className="text-gray-600">
-                7:00-10:00 -{" "}
-                <strong className="text-gray-800">
-                  Café-da-manhã com ciência!
-                </strong>{" "}
-                O café da manhã será servido na área externa, junto da estação
-                de anilhamento BOA1. Nesse longo café da manhã, o visitante
-                poderá acompanhar o trabalho de pesquisa da equipe de campo do
-                OAMa. Para oferecer o máximo desta experiência, um membro da
-                equipe OAMa estará exclusivamente dedicado a acompanhar o grupo
-                de visitantes, comunicando sobre o trabalho em ação, as aves
-                observadas e o ambiente.
-              </li>
-              <li className="text-gray-600">10:00-12:00 - Livre</li>
-              <li className="text-gray-600">12:00 - Almoço</li>
-              <li className="text-gray-600">14:00-19:00 - Livre</li>
-              <li className="text-gray-600">19:00 - Jantar</li>
-            </ul>
-
-            <h4 className="text-xl text-justify-center text-gray-800 font-bold leading-none mb-1">
-              Domingo{" "}
-            </h4>
-            <ul className="text-gray-600 mb-4 ">
-              <li className="text-gray-600">
-                6:00-10:00 -{" "}
-                <strong className="text-gray-800">Passarinhada!</strong> No dia
-                da passarinhada o café da manhã é pra viagem. Cada um prepara
-                seu lanche, pois o passeio começa cedo. Um membro da equipe OAMa
-                vai guiar o grupo numa passarinhada pelas trilhas da RPPN. Ao
-                longo da manhã o grupo terá oportunidades de parar para
-                descanso, picnic e até um mergulho na piscina natural.
-              </li>
-              <li className="text-gray-600">10:00-12:00 - Check-out</li>
-            </ul>
-          </div>
-          <div className="w-full p-6">
-            <h3 className="text-3xl text-justify-center text-gray-800 font-bold leading-none mb-3">
-              Valores{" "}
-            </h3>
-            <ul className="text-gray-600 mb-4 ">
-              <li className="text-gray-600">
-                <strong className="text-gray-800">
-                  Quarto para uma pessoa:
-                </strong>
-                R$ 900,00
-              </li>
-              <li className="text-gray-600">
-                <strong className="text-gray-800">
-                  Quarto para duas pessoas:
-                </strong>
-                R$ 1.260,00
-              </li>
-            </ul>
+            <p className="mb-4 text-gray-600">
+              {" "}
+             O <strong className="text-gray-800">
+                Café-da-manhã com ciência
+              </strong>{" "}
+              será servido na área externa, junto da estação de
+              anilhamento BOA1. Nesse longo café da manhã, o visitante poderá
+              acompanhar o trabalho de pesquisa da equipe de campo do OAMa. Para
+              oferecer o máximo desta experiência, um membro da equipe OAMa
+              estará exclusivamente dedicado a acompanhar o grupo de visitantes,
+              comunicando sobre o trabalho em ação, as aves observadas e o
+              ambiente.
+            </p>
+            <p className="text-gray-600">
+              No dia da <strong className="text-gray-800">Passarinhada</strong> o café da manhã é pra viagem. Cada um prepara seu
+              lanche, pois o passeio começa cedo. Um membro da equipe OAMa vai
+              guiar o grupo numa passarinhada pelas trilhas da RPPN. Ao longo da
+              manhã o grupo terá oportunidades de parar para descanso, picnic e
+              até um mergulho na piscina natural.
+            </p>
           </div>
           <p className="w-full p-6 text-gray-600">
-            Para agendar sua visita ou saber mais detalhes, escreva para
-            contato@oama.eco.br com o assunto "hospedagem - conservação com
-            ciência" ou entre em contato pelo <a className="text-yellow-600"><Link href="/whatsapp">WhatsApp</Link></a>
+            Agende sua visita preenchendo <a className="text-yellow-600" href="https://docs.google.com/forms/d/e/1FAIpQLScjoSjHExbUrShU2K21bgnahhyKUJ5RhX4NrfP4QzFjDeBxhA/viewform" target="_blank">este formulário</a>. Qualque dúvida, escreva para
+            contato@oama.eco.br ou entre em contato pelo{" "}
+            <a className="text-yellow-600">
+              <Link href="/whatsapp">WhatsApp</Link>.
+            </a>
           </p>
         </div>
       </div>
@@ -245,7 +254,6 @@ const EPCarousel = (props) => {
   );
 };
 
-
 const AniCarousel = (props) => {
   const fotos = [
     {
@@ -257,12 +265,26 @@ const AniCarousel = (props) => {
       legend: "Pesquisador analisa plumagem de ave durante anilhamento.",
     },
     { pic: "/ani_demo/an2.jpg", legend: "Asa aberta de uma saíra-lagarta." },
-    { pic: "/ani_demo/an4.jpg", legend: "Pesquisadora aplica anilha em um sabiá-de-coleira." },
-    { pic: "/ani_demo/an5.jpg", legend: "Pesquisador fazendo extração de ave da rede de neblina - foto Jorge Lucas (Birds Atlantic)." },
-    { pic: "/ani_demo/an6.jpg", legend: "Pesquisadora fotografando um trinca-ferro durante o anilhamento - foto Jorge Lucas (Birds Atlantic)." },
+    {
+      pic: "/ani_demo/an4.jpg",
+      legend: "Pesquisadora aplica anilha em um sabiá-de-coleira.",
+    },
+    {
+      pic: "/ani_demo/an5.jpg",
+      legend:
+        "Pesquisador fazendo extração de ave da rede de neblina - foto Jorge Lucas (Birds Atlantic).",
+    },
+    {
+      pic: "/ani_demo/an6.jpg",
+      legend:
+        "Pesquisadora fotografando um trinca-ferro durante o anilhamento - foto Jorge Lucas (Birds Atlantic).",
+    },
 
-    { pic: "/ani_demo/an7.jpg", legend: "Pesquisador liberando tiê-de-coroa após anilhamento - foto Jorge Lucas (Birds Atlantic)." },
-    
+    {
+      pic: "/ani_demo/an7.jpg",
+      legend:
+        "Pesquisador liberando tiê-de-coroa após anilhamento - foto Jorge Lucas (Birds Atlantic).",
+    },
   ];
 
   return (
