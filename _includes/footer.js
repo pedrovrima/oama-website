c6c62a939d23f376e2c385b954bca10c6c59c375
Pedro Martins <pedrovrima@gmail.com>
2022-07-14 11:55:14 -0300

    change icons and add whatsapp

--- _includes/footer.js
+++ _includes/footer.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+
 export default function Footer() {
   return (
     <footer>
@@ -11,37 +17,33 @@ export default function Footer() {
           <p> contato@oama.eco.br</p>
         </div>
         <div className="flex  flex-col justify-center w-full md:w-1/2">
-          <h1 className="hidden md:block font-bold w-full text-center text-lg text-gray-100">Redes Sociais</h1>
+          <h1 className="hidden md:block font-bold w-full text-center text-lg text-gray-100">
+            Redes Sociais
+          </h1>
 
           <div className="mt-4 md:m-0 flex flex-row  justify-center items-center">
             <div className="m-2">
-              <a
-                target="_blank"
-                href="https://www.facebook.com/oamantiqueira"
-              >
-                <Image
-                alt="logo do Facebook"
-                  className="filter invert cursor-pointer"
-                  // layout="responsive"
-                  width={25}
-                  height={25}
-                  src="/logos/facebook.svg"
-                ></Image>
+              <a target="_blank" href="https://www.facebook.com/oamantiqueira">
+                <AiOutlineFacebook color="white" size={30}></AiOutlineFacebook>{" "}
               </a>
             </div>
             <div className="m-2">
-              <a
-                target="_blank"
-                href="https://www.instagram.com/oamantiqueira"
-              >
-                <Image
-                alt="logo do Instagram"
-                  className="filter invert cursor-pointer"
-                  width={25}
-                  height={25}
-                  src="/logos/instagram.svg"
-                ></Image>
+              <a target="_blank" href="https://www.instagram.com/oamantiqueira">
+                <AiOutlineInstagram
+                  color="white"
+                  size={30}
+                ></AiOutlineInstagram>{" "}
               </a>
+            </div>
+            <div className="cursor-pointer m-2">
+              <Link passHref href="/whatsapp">
+                <a target="_blank">
+                  <AiOutlineWhatsApp
+                    color="white"
+                    size={30}
+                  ></AiOutlineWhatsApp>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
