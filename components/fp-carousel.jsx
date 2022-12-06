4b5f7ee8b471344e70b8df3d1caf99bddc897aa7
Pedro Martins <pedro.martins@criticalmass.com>
2022-12-06 10:18:30 -0300

    teste2

--- components/fp-carousel.jsx
+++ components/fp-carousel.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import Link from "next/link";
 
-const EPCarousel = (props) => {
+const FPCarousel = (props) => {
   const fotos = [
     {
       pic: "/fotos_ep/aerea.jpeg",
@@ -23,7 +23,7 @@ const EPCarousel = (props) => {
   ];
 
   return (
-    <div className="flex group cursor-pointer justify-center items-center sm:px-8">
+    <div className="flex group h-100 cursor-pointer justify-center items-center sm:px-8">
       <Carousel
         showArrows={true}
         showStatus={false}
@@ -57,4 +57,49 @@ const EPCarousel = (props) => {
   );
 };
 
-export { EPCarousel };
+const EPCarousel = (props) => {
+  const fotos = [
+    { pic: "/fotos_ep/aerea.jpeg", legend: "Foto aérea da EP." },
+    { pic: "/fotos_ep/frontal.jpg", legend: "Foto frontal da EP." },
+    { pic: "/fotos_ep/lateral.jpg", legend: "Vista lateral da EP." },
+    { pic: "/fotos_ep/piscina.jpg", legend: "Piscina Natural." },
+
+    { pic: "/fotos_ep/acesso.jpg", legend: "Trilha de acesso à EP." },
+    {
+      pic: "/fotos_ep/noite.jpg",
+      legend: "Vista noturna da EP, com Pedra Selada ao fundo.",
+    },
+  ];
+
+  return (
+    <div className="flex group h-100 w-72 justify-center items-center sm:px-8">
+      <Carousel
+        showArrows={true}
+        showStatus
+        autoPlay
+        swipeable
+        emulateTouch
+        infiniteLoop
+        className="mt-2 bg-gray-700 mb-8 sm:mb-4  h-72 w-full sm:h-96 my-auto"
+      >
+        {fotos.map((img) => (
+          <div className="h-72 w-full sm:h-96  relative">
+            <Image
+              alt={img.legend}
+              layout="fill"
+              objectFit="contain"
+              src={img.pic}
+            ></Image>
+            <div className="w-full flex absolute bottom-12 justify-center">
+              <p className="w-1/3 opacity-80 sm:opacity-5 text-white bg-gray-700 rounded-md p-2 group-hover:opacity-80 ">
+                {img.legend}
+              </p>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export { EPCarousel, FPCarousel };
