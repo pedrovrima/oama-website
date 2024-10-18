d4cc785a6133e931f606260263a7262353100484
Pedro Martins <pedrovrima@gmail.com>
2024-10-18 01:00:15 -0300

    fix layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,52 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&display=swap'
-            rel='stylesheet'
-          />
-          <link
-            href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700'
-            rel='stylesheet'
-          />
-          <script
-            async
-            src='https://www.googletagmanager.com/gtag/js?id=G-RFRQHFHPN8'
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-RFRQHFHPN8', {
-                page_path: window.location.pathname,
-              });
-          `,
-            }}
-          />
-
-          {/* <script src="https://www.paypal.com/sdk/js?client-id=Ab2lPDlKKAMdeKZpmz1wFW2_HYyjuhm1rzPjz3nAleWM4PaAEu5U6TNKwBzMee9NJzJHCpq1qA1hr0rx&currency=BRL" /> */}
-        </Head>
-        <body
-          className='bg-white'
-          style={{ fontFamily: 'Source Sans Pro, sans-serif' }}
-        >
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body>
+        {/* Layout UI */}
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 }
-
-export default MyDocument;
