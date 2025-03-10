98bf3c7aa22382c2f9dba0f244b99083b8b1d50b
Pedro Martins <pedrovrima@gmail.com>
2025-03-10 11:11:08 -0300

    fix front page carrousel

--- components/fp-carousel.jsx
+++ components/fp-carousel.jsx
@@ -1,65 +1,66 @@
-import { Carousel } from "react-responsive-carousel";
-import Image from "next/legacy/image";
-import Link from "next/link";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/legacy/image';
+import Link from 'next/link';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const FPCarousel = (props) => {
   const fotos = [
     {
-      pic: "/carousel/treinamento.jpg",
-      legend: "Treinamento 2024",
-      link: "treinamento-cursos#acc2",
+      pic: '/carousel/treinamento.jpg',
+      legend: 'Programa de Treinamento',
+      link: 'treinamento-cursos#acc2',
       legendText:
-        "Inscrições abertas para III temporada de Programa de Treinamento em Monitoramento de Avifauna",
+        'Um programa intensivo em técnicas de monitoramento de aves único no Brasil',
     },
-    {
-      pic: "/carousel/cursos.jpg",
-      legend: "Cursos 2024",
-      link: "/treinamento-cursos#curso2024",
-      legendText:
-        "Estão abertas as inscrições para os cursos OAMa 2024",
-    },
-    
-    {
-      pic: "/carousel/banner-percepcao.jpg",
-      legend: "Pesquisa de Percepção",
-      link: "https://bit.ly/4cfSNFs?r=qr",
-      legendText:
-        "A sua opinião importa, e nós queremos te ouvir!",
-    },
-    {
-      pic: "/carousel/1.jpg",
-      legend: "Portfólio OAMa",
-      link: "/publicacoes/files/Portfolio_OAMa.pdf",
-      legendText:
-        "Conheça e compartilhe nossos objetivos, projetos, programas e conquistas.",
-    },
-    {
-      pic: "/carousel/2.jpg",
-      legend: "Campanha Jacuçara",
-      link: "/jacucara",
-      legendText:
-        "Uma ação pela divulgação de duas espécies ameaçadas e exclusivas da Mata Atlântica: a jacutinga e a palmeira-juçara.",
-    },
-    {
-      pic: "/carousel/3.jpg",
-      legend: "Campanha Pró-Aves",
-      link: "/proaves",
-      legendText:
-        "Aprenda sobre seis impactos que a humanidade gera sobre as aves e o que nós podemos fazer para mitigá-los.",
-    },
-    {
-      pic: "/carousel/4.jpg",
 
-      legend: "Apoio Comunitário",
-      link: "/apoie",
+    {
+      pic: '/carousel/cursos.jpg',
+      legend: 'Cursos Técnicos',
+      link: '/treinamento-cursos#curso2024',
       legendText:
-        "Essa é a força motora que dá vida às nossas ações. Junte-se ao nosso bando pela Conservação com Ciência.",
+        'Capacitação de excelência para uma ciência de qualidade e bem-estar animal',
+    },
+    {
+      pic: '/carousel/3.jpg',
+      legend: 'Programa Ações Pró-Aves',
+      link: '/proaves',
+      legendText:
+        'Aprenda sobre seis impactos que a humanidade gera sobre as aves e o que nós podemos fazer para mitigá-los.',
+    },
+    {
+      pic: '/carousel/banner-percepcao.jpg',
+      legend: 'Pesquisa de Percepção',
+      link: 'https://www.oama.eco.br/publicacoes/files/OAMa_relatorio_percepcao.pdf',
+      legendText:
+        'O que a sociedade pensa sobre a conservação da Mata Atlântica',
+    },
+    {
+      pic: '/carousel/1.jpg',
+      legend: 'Portfólio OAMa',
+      link: '/publicacoes/files/Portfolio_OAMa.pdf',
+      legendText:
+        'Conheça e compartilhe nossos objetivos, projetos, programas e conquistas.',
+    },
+    {
+      pic: '/carousel/2.jpg',
+      legend: 'Campanha Jacuçara',
+      link: '/jacucara',
+      legendText:
+        'Uma ação pela divulgação de duas espécies ameaçadas e exclusivas da Mata Atlântica: a jacutinga e a palmeira-juçara.',
+    },
+
+    {
+      pic: '/carousel/4.jpg',
+
+      legend: 'Apoio Comunitário',
+      link: '/apoie',
+      legendText:
+        'Essa é a força motora que dá vida às nossas ações. Junte-se ao nosso bando pela Conservação com Ciência.',
     },
   ];
 
   return (
-    <div className="flex items-center justify-center cursor-pointer group h-100 sm:px-8">
+    <div className='h-100 group flex cursor-pointer items-center justify-center sm:px-8'>
       <Carousel
         showArrows={true}
         showStatus={false}
@@ -70,24 +71,24 @@ const FPCarousel = (props) => {
         // showThumbs={false}
         stopOnHover
         transitionTime={600}
-        className="w-full mt-2 mb-8 bg-gray-300 sm:mb-4 h-96"
+        className='mb-8 mt-2 h-96 w-full bg-gray-300 sm:mb-4'
       >
         {fotos.map((img) => (
           <Link key={img.link} href={img.link} legacyBehavior>
-            <div className="relative w-full h-96 sm:h-96">
+            <div className='relative h-96 w-full sm:h-96'>
               <Image
                 alt={img.legend}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="top"
+                layout='fill'
+                objectFit='cover'
+                objectPosition='top'
                 src={img.pic}
               ></Image>
-              <div className="absolute w-full h-full bg-gradient-to-b to-gray-900 via-transparent from-transparent t-0"></div>
-              <div className="absolute bottom-0 flex flex-col items-start justify-start w-full px-2 pb-8 md:px-12 md:pb-12">
-                <p className="text-3xl font-bold text-white md:text-6xl">
+              <div className='t-0 absolute h-full w-full bg-gradient-to-b from-transparent via-transparent to-gray-900'></div>
+              <div className='absolute bottom-0 flex w-full flex-col items-start justify-start px-2 pb-8 md:px-12 md:pb-12'>
+                <p className='text-3xl font-bold text-white md:text-6xl'>
                   {img.legend}
                 </p>
-                <p className="text-lg text-left text-white md:text-xl">
+                <p className='text-left text-lg text-white md:text-xl'>
                   {img.legendText}
                 </p>
               </div>
